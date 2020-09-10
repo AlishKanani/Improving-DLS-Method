aed2c33c9abd9fa37e29e6f94cbde00fd58d7bd1
AlishKanani <aliskanani@gmail.com>
2020-09-11 02:21:46 +0530

    will save list in yaml format

--- src/separate_non_DL_matches.py
+++ src/separate_non_DL_matches.py
@@ -1,21 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Aug 25 20:17:28 2020
+import yaml, os
 
-@author: Swar Vaidya
-"""
+data_path= os.getcwd()+'/../data/test/'
 
+files = [f for f in os.listdir(data_path) if f.endswith(".yaml")]
+match_list=[]
 
-import yaml
-from os import listdir
-from os.path import isfile, join
-mypath= r'C:\Users\acer\Desktop\Bhanvaanu\DL\Match Data'
-output_filename='Matches without use of DL method.txt'
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-for file_number in range(len(onlyfiles)):
-    with open(r'C:\Users\acer\Desktop\Bhanvaanu\DL\Match Data\{filename}'.format(filename=onlyfiles[file_number])) as file:
-        match = yaml.load(file, Loader=yaml.FullLoader)
-    if(match.get('info').get('outcome').get('method') == None):
-        if(match.get('info').get('outcome').get('winner') != None):
-            print(onlyfiles[file_number], file=open(output_filename, 'a'))
-    
+for file_number in files:
+    file_obj = open(data_path+file_number)
+    match = yaml.load(file_obj, Loader=yaml.FullLoader)
+    outcome = match.get('info').get('outcome')
+    if outcome.get('method')==None and outcome.get('winner')!=None:
+        match_list.append(file_number)
+    file_obj.close()
+
+with open('test_list.yaml', 'w') as outfile:
+    yaml.dump(match_list, outfile, default_flow_style=False)
